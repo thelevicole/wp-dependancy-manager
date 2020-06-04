615d4dbf1f37f462698b8b9c74442850dc91d9f2
Levi Cole <levi@skape.co>
2020-06-04 13:42:18 +0100

    Added cache file methods

--- src/Core/Asset.php
+++ src/Core/Asset.php
@@ -35,6 +35,22 @@ class Asset {
 	}
 
 	/**
+	 * Create a hash string from properties
+	 *
+	 * @return string
+	 */
+	public function hash() {
+
+		$parts = array_filter( [
+			$this->type(),
+			$this->handle(),
+			$this->source()
+		] );
+
+		return md5( implode( '-', $parts ) );
+	}
+
+	/**
 	 * Check if the asset is a specific type
 	 *
 	 * @param  string  $type
@@ -48,6 +64,35 @@ class Asset {
 		}
 
 		return false;
+	}
+
+	/**
+	 * Check if asset source is external
+	 *
+	 * @return boolean
+	 */
+	public function is_external() {
+
+		$source = $this->source();
+		$compare = get_site_url();
+
+		$src_parts		= parse_url( $source );
+		$src_fragment	= !empty( $src_parts[ 'fragment' ] ) ? $src_parts[ 'fragment' ] : null;
+		$src_path		= !empty( $src_parts[ 'path' ] ) ? $src_parts[ 'path' ] : null;
+		$src_host		= !empty( $src_parts[ 'host' ] ) ? $src_parts[ 'host' ] : null;
+
+		$compare_parts		= parse_url( $compare );
+		$compare_fragment	= !empty( $compare_parts[ 'fragment' ] ) ? $compare_parts[ 'fragment' ] : null;
+		$compare_path		= !empty( $compare_parts[ 'path' ] ) ? $compare_parts[ 'path' ] : null;
+		$compare_host		= !empty( $compare_parts[ 'host' ] ) ? $compare_parts[ 'host' ] : null;
+
+
+		// Link is either #link or /link
+		if ( $src_fragment || $src_path ) {
+			return $src_host ? strpos( $src_host, $compare_host ) === false : false;
+		}
+
+		return strpos( $tis->srce, $compare_host ) === false;
 	}
 
 	/**
@@ -74,21 +119,49 @@ class Asset {
 	}
 
 	/**
-	 * Build a absolute cache path (rel to host root)
+	 * Build a absolute cache directory path (rel to host root)
 	 *
 	 * @return string
 	 */
-	public function get_cache_path() {
+	public function get_cache_dir_path() {
 		return rtrim( WP_CONTENT_DIR, DIRECTORY_SEPARATOR ) . $this->get_cache_dir();
 	}
 
 	/**
-	 * Get the public cache url
+	 * Get the public cache directory url
 	 *
 	 * @return string
 	 */
-	public function get_cache_url() {
+	public function get_cache_dir_url() {
 		return content_url( $this->get_cache_dir() );
+	}
+
+	/**
+	 * Get the cached file name including extension
+	 *
+	 * @return string
+	 */
+	public function get_cache_file_name() {
+		$ext = pathinfo( parse_url( $this->source(), PHP_URL_PATH ), PATHINFO_EXTENSION );
+		return $this->hash() . '.' . $ext;
+	}
+
+	/**
+	 * Build a absolute cache file path (rel to host root)
+	 *
+	 * @return string
+	 */
+	public function get_cache_file_path() {
+		return $this->get_cache_dir_path() . $this->get_cache_file_name();
+	}
+
+	/**
+	 * Get the public cache file url
+	 *
+	 * @return string
+	 */
+	public function get_cache_file_url() {
+		return content_url( $this->get_cache_dir() . $this->get_cache_file_name() );
 	}
 
 	/**
@@ -161,48 +234,13 @@ class Asset {
 	}
 
 	/**
-	 * Check if asset source is external
+	 * Get the file contents from cache path
 	 *
-	 * @return boolean
+	 * @return ?string
 	 */
-	public function is_external() {
-
-		$source = $this->source();
-		$compare = get_site_url();
-
-		$src_parts		= parse_url( $source );
-		$src_fragment	= !empty( $src_parts[ 'fragment' ] ) ? $src_parts[ 'fragment' ] : null;
-		$src_path		= !empty( $src_parts[ 'path' ] ) ? $src_parts[ 'path' ] : null;
-		$src_host		= !empty( $src_parts[ 'host' ] ) ? $src_parts[ 'host' ] : null;
-
-		$compare_parts		= parse_url( $compare );
-		$compare_fragment	= !empty( $compare_parts[ 'fragment' ] ) ? $compare_parts[ 'fragment' ] : null;
-		$compare_path		= !empty( $compare_parts[ 'path' ] ) ? $compare_parts[ 'path' ] : null;
-		$compare_host		= !empty( $compare_parts[ 'host' ] ) ? $compare_parts[ 'host' ] : null;
-
-
-		// Link is either #link or /link
-		if ( $src_fragment || $src_path ) {
-			return $src_host ? strpos( $src_host, $compare_host ) === false : false;
-		}
-
-		return strpos( $tis->srce, $compare_host ) === false;
-	}
-
-	/**
-	 * Create a hash string from properties
-	 *
-	 * @return string
-	 */
-	public function hash() {
-
-		$parts = array_filter( [
-			$this->type(),
-			$this->handle(),
-			$this->source()
-		] );
-
-		return md5( implode( '-', $parts ) );
+	public function cache_contents() {
+		$contents = @file_get_contents( $this->get_cache_file_path() );
+		return $contents !== false ? $contents : null;
 	}
 
 	/**
@@ -212,18 +250,14 @@ class Asset {
 	 */
 	public function source_contents() {
 		$contents = @file_get_contents( $this->source() );
-
-		if ( !empty( $contents ) ) {
-			return $contents;
-		}
-
-		return null;
+		return $contents !== false ? $contents : null;
 	}
 
-	public function cache_contents() {
-		return null;
-	}
-
+	/**
+	 * Get the contents from either cache or source
+	 *
+	 * @return ?string
+	 */
 	public function contents() {
 		return $this->cache_contents() ?: $this->source_contents();
 	}
